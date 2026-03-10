89cfcc1c4dcab6c941ad7826b6a9e8d3b1b671d2
SatohAyaka <sayaka310ss@docomo.ne.jp>
2026-03-10 12:30:01 +0900

    fix: 削除を許可しない形に

--- app/router/router.go
+++ app/router/router.go
@@ -12,7 +12,7 @@ func Router() {
 	r.Use(func(c *gin.Context) {
 		// c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://leaving-match.vercel.app")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		c.Next()
 	})
